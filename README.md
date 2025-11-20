2a5a879e6345fee4710ca237222036c0c9173175
magdiel uriel cruz reyna <magdielcruz278@gmail,com>
2025-11-20 11:29:59 -0600

    solo muestra la lista de compras

---
+++ README.md
@@ -0,0 +1,1 @@
+aplicación de lista de compras 
---
+++ ShopList.Gui/ViewModels/ShopListViewModel.cs
@@ -0,0 +1,42 @@
+﻿using ShopList.Gui.Models;
+using System.Collections.ObjectModel;
+
+
+namespace ShopList.Gui.ViewModels
+{
+    internal class ShopListViewModel
+    {
+        public ObservableCollection<Item> Items { get; }
+
+        public ShopListViewModel()
+        {
+            Items = new ObservableCollection<Item>();
+            CargarDatos();
+        }
+
+        public void CargarDatos()
+        {
+            Items.Add(new Item()
+            {
+                Id = 1,
+                Nombre = "Leche",
+                Cantidad = 2
+            });
+
+            Items.Add(new Item()
+            {
+                Id = 2,
+                Nombre = "Pan de caja",
+                Cantidad = 1
+            });
+
+            Items.Add(new Item()
+            {
+                Id = 3,
+                Nombre = "Jamón",
+                Cantidad = 500
+            });
+
+        }
+    }
+}
